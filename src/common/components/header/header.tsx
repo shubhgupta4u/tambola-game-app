a46aadeddf35cfa3921165b7476c75937ada2034
Shubh Gupta <shubhgupta4u@gmail.com>
2024-04-21 17:31:14 +0530

    firebase and dockerfile added

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -3,6 +3,7 @@ import styles from "@/common/components/header/header.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTicket, faKeyboard, faPersonCircleQuestion } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
+import Link from 'next/link';
 
 export function Header() { 
     const pathname = usePathname();
@@ -24,7 +25,7 @@ export function Header() {
             <nav className={`${styles.navbar} navbar-expand-lg navbar-dark mx-background-top-linear ${styles.mxBackgroundTopLinear}`}>
 
                 <div className={`container ${styles.navbarContainer}`}>
-                    <a className={`navbar-brand ${styles.navbarBrandText}`} href="/">Tambola.in</a>
+                    <Link className={`navbar-brand ${styles.navbarBrandText}`} href="/">Tambola.in</Link>
                     <button className={`navbar-toggler ${styles.mlAuto}`} type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={()=>{toggleMenu()}}>
                         <span className="navbar-toggler-icon"></span>
@@ -46,10 +47,10 @@ export function Header() {
                                             :
                                             (
                                                 <li key={link.href} className={`nav-item ${styles.headerList}`}>
-                                                    <a className={`nav-link ${styles.headerLink}`}
+                                                    <Link className={`nav-link ${styles.headerLink}`}
                                                         href={pathname == link.href ? "" : link.href}>
                                                         <FontAwesomeIcon icon={link.icon} />  {link.name}
-                                                    </a>
+                                                    </Link>
                                                 </li>
                                             )
                                     )
